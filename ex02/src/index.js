9f8c185da15460252937aee64e5f33c2b1390bff
alko8910 <almedinemkic@gmail.com>
2021-03-13 10:03:33 +0100

    Added Day05

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  // Only change code below this line
+  var myLink = '<a href="https://www.example.com" target="_blank">link</a>';
+  // Only change code above this line
+
+  return myLink;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex14/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+  // Only change code below this line
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks =
+    "I have a " +
+    myNoun +
+    " and he " +
+    myVerb +
+    ". He is " +
+    myAdjective +
+    " but he is realy " +
+    myAdverb +
+    ".";
+  // Only change code above this line
+
+  return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
